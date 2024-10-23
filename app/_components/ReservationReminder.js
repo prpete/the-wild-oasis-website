bbe7d785f3ba97d0f8558d526c9d1f16e74ed000
CORP\owton <peter.owton@dxcas.com>
2024-10-23 08:51:20 -0700

    replace apostrophe with apos; in ReservationReminder

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -13,7 +13,7 @@ function ReservationReminder() {
   return (
     <div className="fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-colors-accent-600 text-primary-800 text  font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center">
       <p>
-        <span>👋</span> Don'f forget to reserve your dates <br /> from{" "}
+        <span>👋</span> Don&apos;t forget to reserve your dates <br /> from{" "}
         {format(new Date(range.from), "MMM dd yyyy")} to{" "}
         {format(new Date(range.to), "MMM dd yyyy")}
       </p>
